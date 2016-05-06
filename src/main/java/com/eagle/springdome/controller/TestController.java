44c89a24b413f79bed2f2f66ac2219d4c149ccfb
WangYong <topeagle@163.com>
2016-05-06 16:34:14 +0800

    增加jdbc

--- src/main/java/com/eagle/springdome/controller/TestController.java
+++ src/main/java/com/eagle/springdome/controller/TestController.java
@@ -5,6 +5,8 @@ package com.eagle.springdome.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,17 @@ public class TestController {
 
 	private static Logger logger = LoggerFactory.getLogger(TestController.class);
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping("/test.do")
 	public String test(Model model){
 		logger.info("dfgkjdfklgjdfkgj ");
 		model.addAttribute("name","WangYong");
+		int size = jdbcTemplate.queryForList("select * from fee").size();
+		logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		logger.info("size:{}",size);
+		logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 		return "index";
 	}
 	
