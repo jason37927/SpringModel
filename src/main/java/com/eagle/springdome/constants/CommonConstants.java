87fc727a9fbf49783fcfe328b409db405d938dc8
WangYong <topeagle@163.com>
2016-06-16 16:20:26 +0800

    增加基础常量类

---
+++ src/main/java/com/eagle/springdome/constants/CommonConstants.java
@@ -0,0 +1,18 @@
+package com.eagle.springdome.constants;
+
+/**
+ * Created by Wang Yong on 16-6-16.
+ */
+public class CommonConstants{
+
+    /**
+     * 账号正常状态
+     */
+    public static final int ACCOUNT_STATUS_ENABLED = 0;
+
+    /**
+     * 账号锁定状态
+     */
+    public static final int ACCOUNT_STATUS_LOCKED= 1;
+
+}
